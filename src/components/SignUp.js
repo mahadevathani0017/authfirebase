c9116157296ceb1388953ee4a30658efe969b575
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-09-23 23:29:00 +0530

    add bootstrap

---
+++ src/components/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Form, Button, Card } from "react-bootstrap";
+const SignUp= () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default SignUp;
+
