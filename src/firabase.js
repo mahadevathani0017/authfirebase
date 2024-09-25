5068aa5edacd0588f14ab92b7996fddcdcb6ebfd
Ganesh Gowda <135863982+gowdaganesh005@users.noreply.github.com>
2024-09-25 19:07:27 +0530

    Small error in env and firbase file

--- src/firabase.js
+++ src/firabase.js
@@ -4,7 +4,7 @@ import "firbase/auth"
 const app = firebase.initializeApp( {
   apiKey: process.env.REACT_APP_FIREABSE_API_KEY,
   authDomain: process.env.REACT_APP_FIREABSE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREABSE_PROJECT - ID,
+  projectId: process.env.REACT_APP_FIREABSE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREABSE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREABSE_MESSAGEING_SENDER_ID,
   appId: process.env.REACT_APP_FIREABSE_APP_ID,
