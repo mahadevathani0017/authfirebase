0e2c52cc4e6b1ad6641d96a0329018634fab832f
Mahadev Chidanand Athani <athanimahadev348@gmail.com>
2024-09-25 19:30:11 +0530

    fix the some error

--- src/firabase.js
+++ src/firabase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/compat/app";
-import "firbase/auth"
+import firebase from "firebase/app";
+import "firebase/auth";
 
 const app = firebase.initializeApp( {
   apiKey: process.env.REACT_APP_FIREABSE_API_KEY,
   authDomain: process.env.REACT_APP_FIREABSE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREABSE_PROJECT - ID,
+  projectId: process.env.REACT_APP_FIREABSE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREABSE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREABSE_MESSAGEING_SENDER_ID,
   appId: process.env.REACT_APP_FIREABSE_APP_ID,
   databaseURL: process.env.REACT_APP_FIREABSE_DATABASE_URL,
-});
+})
 export const auth =app.auth()
-export default  app
+export default  app;
